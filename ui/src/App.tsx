a22e0cd6b24ed5b4061ed7b53531fb08a8e17486
Seth McCarthy <emperorsheevpalpatine0@gmail.com>
2026-04-29 16:26:10 -0700

    chore(ui): scaffold React + Vite + TS app and base layout

---
+++ ui/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={""} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
