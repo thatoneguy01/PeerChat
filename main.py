9c7fe4711f0069fa1c0e0bf388b838ebac608fa5
hjain3004 <himanshu.jain@sjsu.edu>
2026-05-18 00:55:44 -0700

    reverted logging step in main.py

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ from security.key_storage import InMemoryKeyStore
 from security.persistent_key_storage import get_platform_key_storage
 from security.key_bootstrap import initialize_private_key_store
 from security import configure_private_key
-import logging
 import threading
 import socket, time
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s %(name)s: %(message)s")
 from utils import get_external_ip
 from peer_discovery.network.net_utils import get_lan_ip
 from message_history.storage import HistoryService
@@ -51,8 +49,7 @@ def main():
     node.on_message = lambda msg: app.chat_service.message_received(msg)
     app.chat_service.message_out = lambda content: node.broadcast(Message(content=content, sender=node.address))
     node.start()
-    time.sleep(3)
-    
+
     app.run(debug=True, host="127.0.0.1", port=5050, use_reloader=False)
     # ui_thread = threading.Thread(target=run_ui, args=(app,), kwargs={"debug": True, "host": "127.0.0.1", "port": 5050})
     # ui_thread.daemon = True
